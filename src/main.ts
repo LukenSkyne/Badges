e4effe96ae580e57913035ccd7656f579cf4c1b0
Luca v. d. W <github@luken.cc>
2023-11-27 23:30:08 +0100

    :recycle: use fastify's encapsulation to register routes

--- src/main.ts
+++ src/main.ts
@@ -6,32 +6,28 @@ import { BadgeRenderer } from "./badge-renderer"
 const { HOST, PORT, BASE_URL } = process.env
 const server = fastify({ logger: true })
 
-async function baseRoute(_a: FastifyRequest, _b: FastifyReply) {
-	return "Hello o7"
-}
+server.register(async (s, _) => {
+	s.get("/", (_a: FastifyRequest, _b: FastifyReply) => {
+		return "Hello o7"
+	})
 
-if (BASE_URL !== undefined && BASE_URL !== "") {
-	server.get(BASE_URL, baseRoute)
-}
+	s.get("/favicon.ico", async (_: FastifyRequest, reply: FastifyReply) => {
+		reply.code(404)
+	})
 
-server.get(BASE_URL + "/", baseRoute)
+	s.get("/:preset/:id?", async (request: PresetRequest, reply: FastifyReply) => {
+		const preset = await RequestParser.process(request.params, request.query)
+		console.dir(preset, { depth: null })
 
-server.get(BASE_URL + "/favicon.ico", async (_: FastifyRequest, reply: FastifyReply) => {
-	reply.code(404)
-})
+		const canvas = await BadgeRenderer.render(preset)
 
-server.get(BASE_URL + "/:preset/:id?", async (request: PresetRequest, reply: FastifyReply) => {
-	const preset = await RequestParser.process(request.params, request.query)
-	console.dir(preset, { depth: null })
+		reply.type("image/png")
 
-	const canvas = await BadgeRenderer.render(preset)
+		return canvas.createPNGStream()
+	})
+}, { prefix: BASE_URL })
 
-	reply.type("image/png")
-
-	return canvas.createPNGStream()
-})
-
-server.listen({ host: HOST, port: Number(PORT ?? "3000") }, (err) => {
+server.listen({ host: HOST, port: Number(PORT) }, (err) => {
 	if (err) {
 		console.error(err)
 		process.exit(1)
